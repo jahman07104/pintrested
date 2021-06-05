621117e18de588cfe292eca47ea60adec29b791c
Patrick Harrison <jahman@bellatlantic.net>
2021-06-04 21:02:49 -0400

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  #added as per devise
+  # YOU MUST CHANGE BELOW FOR YOUR WEB HOSTING SITE
+  config.action_mailer.default_url_options = { host: 'pintrested.herokuapp.com'}
 end
