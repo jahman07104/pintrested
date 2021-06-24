24c18651c0df9d2d36567dabd337944c8b2d6383
jahman07104 <jahman@bellatlantic.net>
2021-06-23 20:48:02 -0400

    scaffolded pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_142354) do
+ActiveRecord::Schema.define(version: 2021_06_16_214726) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
